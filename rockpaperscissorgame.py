e19e0adfa80b976b014ead6a2c367e0f986321cb
WrightS95 <60717800+WrightS95@users.noreply.github.com>
2020-03-08 20:51:54 -0500

    no need to declare ints; made while loop and game results more readable

--- rockpaperscissorgame.py
+++ rockpaperscissorgame.py
@@ -2,36 +2,37 @@ import random
 
 # CREATED 12.21.19
 hands = ["rock", "paper", "scissors"]
-choices = int(0)
-tries = int(3)
-player_points = int(0)
-computer_points = int(0)
+choices = 0
+tries = 3
+player_points = 0
+computer_points = 0
 # goes up to five rounds for player
 while player_points != 5:
     computer_choice = random.choice(hands)
-    player_choice = input("Please choose rock, paper, or scissors").lower().strip()
+    player_choice = input("Please choose rock, paper, or scissors: ").lower().strip()
     if player_choice == computer_choice:  # computer and player choice are same
+        print(computer_choice)
         print("Try again, you guys got the same thing!\n")
-    elif player_choice == "rock":  # player chooses rock.
-        if computer_choice == "scissors":
+    elif player_choice == "rock":  #-----------------------------player choice: rock
+        if computer_choice == "scissors": #computer choice scissors
             player_points += 1
-            print(player_choice, "beats: ", computer_choice, ". You won this round.\n")
+            print(f"Player choice: {player_choice}. Computer choice: {computer_choice}. You won this round.")
         else:  # computer chose paper
             computer_points += 1
-            print("Computer chose: ", computer_choice, "Better luck next time.\n")
-    elif player_choice == "paper":  # player chooses paper
-        if computer_choice == "rock":
+            print(f"Player choice: {player_choice}. Computer choice: {computer_choice}. Better luck next time.")
+    elif player_choice == "paper":  #-----------------------------player choice: paper
+        if computer_choice == "rock": #computer choice: rock
             computer_points += 1
-            print("Computer chose: ", computer_choice, "Better luck next time!\n")
+            print(f"Player choice: {player_choice}. Computer choice: {computer_choice}. Better luck next time!")
         else:  # computer chose scissors
             player_points += 1
-            print(player_choice, "beats: ", computer_choice, "You won this round.\n")
+            print(f"Player choice: {player_choice}. Computer choice: {computer_choice}. You won this round.")
     elif player_choice == "scissors":  # player chooses scissors
         if computer_choice == "rock":
-            print("Computer chose:", computer_choice, ". Better luck next time!\n")
+            print(f"Player choice: {player_choice}. Computer choice: {computer_choice}. Better luck next time!")
         else:  # computer chose paper, you chose scissors:
             player_points += 1
-            print(player_choice, "beats", computer_choice, "You won this round.\n")
+            print(f"Player choice: {player_choice}. Computer choice: {computer_choice}. You won this round.")
     else:  # player chooses something that is not paper, scissors, or rock
         print("Please choose rock, paper, scissors.\n")
 

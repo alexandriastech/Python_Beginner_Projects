4a9cc5aee8fcad077435617f1c81923ca5cdeba9
Peter490 <72179316+Peter490@users.noreply.github.com>
2020-10-01 10:01:10 +0530

    Update rockpaperscissorgame.py

--- rockpaperscissorgame.py
+++ rockpaperscissorgame.py
@@ -2,12 +2,12 @@ import random
 
 # CREATED 12.21.19
 hands = ["rock", "paper", "scissors"]
-choices = int(0)
-tries = int(3)
-player_points = int(0)
-computer_points = int(0)
+choices = 0
+player_points = 0
+computer_points = 0
+rounds = 5
 # goes up to five rounds for player
-while player_points != 5:
+while rounds > 0:
     computer_choice = random.choice(hands)
     player_choice = input("Please choose rock, paper, or scissors").lower().strip()
     if player_choice == computer_choice:  # computer and player choice are same
@@ -16,24 +16,32 @@ while player_points != 5:
         if computer_choice == "scissors":
             player_points += 1
             print(player_choice, "beats: ", computer_choice, ". You won this round.\n")
+            rounds -= 1
         else:  # computer chose paper
             computer_points += 1
             print("Computer chose: ", computer_choice, "Better luck next time.\n")
+            rounds -= 1
     elif player_choice == "paper":  # player chooses paper
         if computer_choice == "rock":
             computer_points += 1
             print("Computer chose: ", computer_choice, "Better luck next time!\n")
+            rounds -= 1
         else:  # computer chose scissors
             player_points += 1
             print(player_choice, "beats: ", computer_choice, "You won this round.\n")
+            rounds -= 1
     elif player_choice == "scissors":  # player chooses scissors
         if computer_choice == "rock":
             print("Computer chose:", computer_choice, ". Better luck next time!\n")
+            rounds -= 1
         else:  # computer chose paper, you chose scissors:
             player_points += 1
             print(player_choice, "beats", computer_choice, "You won this round.\n")
+            rounds -= 1
     else:  # player chooses something that is not paper, scissors, or rock
         print("Please choose rock, paper, scissors.\n")
 
-print("Congratulations, you've won a total of: ", player_points, "rounds")
-print("The computer has won: ", computer_points, "rounds")
+if player_points > computer_points:
+    print(f'Congratulations, You won the game.\nYou got {player_points} points.\nComputer got {computer_points} points.')
+elif computer_points > player_points:
+    print(f'Better luck next time, You lose the game.\nYou got {player_points} points.\nComputer got {computer_points} points.')
